4846f4b7ebf29e4a5b90c8571f5deed9e7c7c4cf
Kevin Porras <kevin.porras@pantheon.io>
2023-03-08 07:23:04 -0600

    Use verbose mode.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ composer install
 
 export PATH=$PATH:$GITHUB_ACTION_PATH/vendor/bin
 
-./vendor/bin/phpcs $PATHS --standard=PHPCompatibility --runtime-set testVersion $TEST_VERSIONS
+./vendor/bin/phpcs -v $PATHS --standard=PHPCompatibility --runtime-set testVersion $TEST_VERSIONS
